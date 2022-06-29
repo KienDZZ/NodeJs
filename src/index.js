2f7ab7e2d14a3e05c621d4db44522712c756aeb3
@kien.letrung <kien.letrung@vti.vn>
2022-06-29 17:29:29 +0700

    start lesson 11 use bootstrap

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources\\views'));
 console.log('dssds',path.join(__dirname,'resources\\views'));
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
